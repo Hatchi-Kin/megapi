7522455f3bade88650b998047d4be321e4b52d7e
Hatchi-Kin <j.needham@hotmail.fr>
2024-03-31 12:42:13 +0200

    modif list all albums from artist

--- routes/milvus.py
+++ routes/milvus.py
@@ -98,6 +98,27 @@ def get_similar_entities_by_path(query: FilePathsQuery, user=Depends(login_manag
     return SimilarFullEntitiesResponse(hits=response_list)
 
 
+# @router.post("/similar_short_entity", tags=["milvus"], response_model=SimilarShortEntitiesResponse)
+# def get_similar_9_entities_by_path(query: FilePathsQuery, user=Depends(login_manager)):
+#     """Get the 9 most similar (title, artist, album)) to the entity with the given file path."""
+#     collection_512 = get_milvus_collection()
+#     entities = collection_512.query(expr=f"path in {query.path}", output_fields=["embedding"])
+#     if not entities:
+#         raise HTTPException(status_code=404, detail="Entity not found")
+    
+#     embeddings = [[float(x) for x in entity["embedding"]] for entity in entities]
+#     entities = collection_512.search(
+#         data=embeddings,
+#         anns_field="embedding",
+#         param={"nprobe": 16},
+#         limit=9,
+#         offset=1,
+#         output_fields=["title", "album", "artist", "path"],
+#     )
+
+#     response_list = [short_hit_to_dict(hit) for hit in entities[0]]
+#     return {"entities": response_list}
+
 @router.post("/similar_short_entity", tags=["milvus"], response_model=SimilarShortEntitiesResponse)
 def get_similar_9_entities_by_path(query: FilePathsQuery, user=Depends(login_manager)):
     """Get the 9 most similar (title, artist, album)) to the entity with the given file path."""
@@ -111,10 +132,26 @@ def get_similar_9_entities_by_path(query: FilePathsQuery, user=Depends(login_man
         data=embeddings,
         anns_field="embedding",
         param={"nprobe": 16},
-        limit=9,
+        limit=50,  # Increase limit to ensure we get enough results
         offset=1,
         output_fields=["title", "album", "artist", "path"],
     )
 
-    response_list = [short_hit_to_dict(hit) for hit in entities[0]]
+    recommended_artists = set()
+    response_list = []
+    fallback_list = []
+    for hit in entities[0]:
+        hit_dict = short_hit_to_dict(hit)
+        if hit_dict["artist"] not in recommended_artists:
+            response_list.append(hit_dict)
+            recommended_artists.add(hit_dict["artist"])
+        else:
+            fallback_list.append(hit_dict)
+        if len(response_list) == 9:  # Stop when we have 9 results
+            break
+
+    # If we have less than 9 results, add from fallback_list
+    if len(response_list) < 9:
+        response_list.extend(fallback_list[:9-len(response_list)])
+
     return {"entities": response_list}
