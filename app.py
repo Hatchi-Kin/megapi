9d86b4bbf28497e544657ecd6808466427bb09df
Hatchi-Kin <j.needham@hotmail.fr>
2024-04-18 21:11:48 +0200

    test

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from routes.milvus import router as milvus_router
 from core.config import Base, engine, swagger_tags
 from core.database import migrate_data_from_sqlite_to_postgres, create_admin_if_none
 
-
+### TEST ###
 app = FastAPI(
     title="Megapi",
     description="Right now, a simple login system using FastAPI and FastAPI-Login... but just you wait !",
