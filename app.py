a7dab851a31bda536b29c84320f04fc10b47622a
Hatchi-Kin <j.needham@hotmail.fr>
2024-10-19 20:02:59 +0200

    updt add_middleware

--- app.py
+++ app.py
@@ -31,7 +31,8 @@ app.add_middleware(
     allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],  
-    allow_headers=["*"],  
+    allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 app.add_middleware(AuthMiddleware)
