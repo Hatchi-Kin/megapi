e8a59301d29faaaeda13ce99a6a748ed52b000d8
Hatchi-Kin <j.needham@hotmail.fr>
2024-10-31 19:21:39 +0000

    add exceptions to elo.py

--- routes/elo.py
+++ routes/elo.py
@@ -1,15 +1,19 @@
 import os
+
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.responses import JSONResponse
+
 from models.music import SongPath
 from core.config import login_manager, DEFAULT_SETTINGS
 from services.minio import get_temp_file_from_minio, get_metadata_and_artwork
 from services.milvus import get_milvus_87_collection, extract_plot_data, create_plot, convert_plot_to_base64
 from services.music_net import create_preprocessed_spectrogram, get_production_model, predict_with_production_music_net
 
+
 router = APIRouter(prefix="/elo")
 
-async def get_model_predictions(file_path: str):
+
+async def get_mlflow_model_predictions(file_path: str):
     model = get_production_model()
     if model is None:
         raise HTTPException(status_code=500, detail="Failed to load the production model.")
@@ -25,32 +29,43 @@ async def get_model_predictions(file_path: str):
     
     return genre
 
+
 async def get_essentia_predictions(file_path: str):
-    collection_87 = get_milvus_87_collection()
-    entity = collection_87.query(
-        expr=f"path == '{file_path}'",
-        output_fields=["predictions", "title", "artist"], 
-        limit=1
-    )
-    if not entity:
-        raise HTTPException(status_code=404, detail="Entity not found")
-    
-    class_names, top_5_activations, title, artist = await extract_plot_data(entity)
-    fig = await create_plot(class_names, top_5_activations, title, artist)
-    predictions_plot = await convert_plot_to_base64(fig)
+    if file_path.startswith("MegaSet/"):
+        collection_87 = get_milvus_87_collection()
+        entity = collection_87.query(
+            expr=f"path == '{file_path}'",
+            output_fields=["predictions", "title", "artist"], 
+            limit=1
+        )
+        if not entity:
+            raise HTTPException(status_code=404, detail="Entity not found")
+        
+        class_names, top_5_activations, title, artist = await extract_plot_data(entity)
+        fig = await create_plot(class_names, top_5_activations, title, artist)
+        predictions_plot = await convert_plot_to_base64(fig)
+
+    else:
+        predictions_plot = None
     
     return predictions_plot
-
 
 
 @router.post("/compare_models", tags=["elo"])
 async def get_comparison(query: SongPath, user=Depends(login_manager)):
     try:
         # 1. Get the metadata and artwork from MinIO
-        metadata = get_metadata_and_artwork(DEFAULT_SETTINGS.minio_bucket_name, query.file_path)
+        if query.file_path.startswith("MegaSet/"):
+            metadata = get_metadata_and_artwork(DEFAULT_SETTINGS.minio_bucket_name, query.file_path)
+        else:
+            metadata = {
+                "file_path": query.file_path,
+                "artist": "Unknown",
+                "artwork": None, 
+            }
 
         # 2. Get the predictions from the model from mlflow and add them to the metadata
-        metadata['prediction_model_1'] = await get_model_predictions(query.file_path)
+        metadata['prediction_model_1'] = await get_mlflow_model_predictions(query.file_path)
 
         # 3. Get the predictions from the model from essentia
         metadata['predictions_openl3'] = await get_essentia_predictions(query.file_path)
@@ -61,3 +76,4 @@ async def get_comparison(query: SongPath, user=Depends(login_manager)):
         raise e
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
+    
